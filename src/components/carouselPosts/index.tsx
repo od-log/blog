f09ee25555e945f934b5a69b884c9da5291c9ad4
OD <kg-te@naver.com>
2023-07-13 00:17:46 +0900

    ✨ CarouselPosts 추가

---
+++ src/components/carouselPosts/index.tsx
@@ -0,0 +1,21 @@
+import { getAllPosts } from '@/service/posts'
+import React from 'react'
+import PostCard from '../postCard';
+import MultiCarousel from '../MultiCarousel';
+
+export default async function CarouselPosts() {
+  const posts = await getAllPosts();
+
+  return (
+    <section className='my-4'>
+      <h2 className='text-2xl font-bold m-2'> You may like </h2>
+      <MultiCarousel>
+        {posts.map((post, index) => {
+          return (
+            <PostCard key={post.id} post={post} />
+          )
+        })}
+      </MultiCarousel>
+    </section>
+  )
+}
