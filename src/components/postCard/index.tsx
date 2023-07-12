e7cb9b88fc332599ddf579da3feb8fd177645332
OD <kg-te@naver.com>
2023-07-13 00:21:10 +0900

    📝 postCard 수정

--- src/components/postCard/index.tsx
+++ src/components/postCard/index.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 import React from 'react'
 
 type Props = { post: Post; }
-export default function PostCard({post : {title, summary, date, path, category}}: Props) {
+export default function PostCard({post : {id, title, description, date, path, category}}: Props) {
   return (
-    <Link href={`/posts/${path}`}>
-      <article className='rounded-md overflow-hidden shadow-2xl'>
+    <Link href={`/posts/${id}`}>
+      <article className='rounded-md overflow-hidden shadow-md hover:shadow-2xl'>
         <Image
           className='w-full'
           src={`/images/${path}.jpg`}
@@ -18,7 +18,7 @@ export default function PostCard({post : {title, summary, date, path, category}}
         <div className='flex flex-col items-center p-4'>
           <time className='self-end'>{date}</time>
           <h3 className='text-lg font-bold'>{title}</h3>
-          <p className='w-full truncate text-center'>{summary}</p>
+          <p className='w-full truncate text-center'>{description}</p>
           <span className='text-sm rounded-lg bg-yellow-400 px-2 m-2'>{category}</span>
         </div>
       </article>
