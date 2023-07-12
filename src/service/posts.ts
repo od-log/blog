3c1096dd8109c2d5245a3821635e988ac730bbad
OD <kg-te@naver.com>
2023-07-12 20:25:11 +0900

    ✨ posts api 추가

---
+++ src/service/posts.ts
@@ -0,0 +1,28 @@
+import path from 'path';
+import { readFile } from 'fs/promises';
+
+export interface Post {
+  id: string;
+  title: string;
+  summary: string;
+  category: string[];
+  date: string;
+  path: string;
+  featured: boolean;
+}
+
+export async function getFeaturedPosts() : Promise<Post[]> {
+  return getAllPosts()
+    .then((posts) => posts.filter((post) => post.featured));
+}
+export async function getAllPosts() : Promise<Post[]> {
+  const filePath = path.join(process.cwd(), 'data', 'posts.json');
+  return readFile(filePath, 'utf-8')
+    .then<Post[]>(JSON.parse)
+    .then((posts) => posts.sort((a,b) => (a.date > b.date ? -1 : 1)));
+}
+
+export async function getPostItem(id: string): Promise<Post | undefined> {
+  const postList = await getAllPosts();
+  return postList.find((item) => item.id === id);
+}
