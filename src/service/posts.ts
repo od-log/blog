791d5a7495e501632afe8255fc9fc1fa59d9ef23
OD <kg-te@naver.com>
2023-07-13 15:28:31 +0900

    ✨ getPostData 추가

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -11,6 +11,8 @@ export interface Post {
   featured: boolean;
 }
 
+export type PostData = Post & { content: string };
+
 export async function getFeaturedPosts() : Promise<Post[]> {
   return getAllPosts()
     .then((posts) => posts.filter((post) => post.featured));
@@ -22,7 +24,15 @@ export async function getAllPosts() : Promise<Post[]> {
     .then((posts) => posts.sort((a,b) => (a.date > b.date ? -1 : 1)));
 }
 
-export async function getPostItem(id: string): Promise<Post | undefined> {
+export async function getPostItem(path: string): Promise<Post | undefined> {
   const postList = await getAllPosts();
-  return postList.find((item) => item.id === id);
+  return postList.find((item) => item.id === path);
+}
+
+export async function getPostData(fileName: string): Promise<PostData> {
+  const filePath = path.join(process.cwd(), 'data','posts', `${fileName}.md`);
+  const metadata = await getAllPosts().then(posts => posts.find(post => post.path === fileName));
+  if (!metadata) throw new Error(`${fileName}`);
+  const content = await readFile(filePath, 'utf-8');
+  return {...metadata, content };
 }
