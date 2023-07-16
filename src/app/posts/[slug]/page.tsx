8a435a46c77a40b44d9b70ad623d01ac6bc7e486
OD <kg-te@naver.com>
2023-07-17 01:44:35 +0900

    📝 PostPage 수정

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,33 +1,24 @@
-import MarkDownViewer from '@/components/MarkDownViewer';
-import { getPostData } from '@/service/posts';
-import Image from 'next/image';
-
+import PostContent from "@/components/postContent";
+import PostPagination from "@/components/postPagination";
+import { getPostData } from "@/service/posts";
 
 type Props = {
   params: {
     slug: string;
-  }
-}
+  };
+};
 
 export default async function PostPage({ params: { slug } }: Props) {
-  const {path, content, title, description, date} = await getPostData(slug);
+  const postData = await getPostData(slug);
+  const { next, prev } = postData;
 
   return (
-    <article className='rounded-2xl overflow-hidden m-4'>
-      <Image
-        className='w-full h-1/6 max-h-[500px]'
-        src={`/images/${path}.jpg`}
-        alt={title}
-        width={760}
-        height={420}
-      />
-      <section className='p-4'>
-        <p className='flex items-center justify-end font-semibold ml-2'>{date.toString()}</p>
-        <h1 className='text-4xl font-bold'>{title}</h1>
-        <p className='text-xl font-bold'>{description}</p>
-        <MarkDownViewer content={content}></MarkDownViewer>
+    <>
+      <PostContent postData={postData} />
+      <section className="flex shadow-md">
+        {prev && <PostPagination post={prev} type={"prev"} />}
+        {next && <PostPagination post={next} type={"next"} />}
       </section>
-    </article>
-  )
+    </>
+  );
 }
-
