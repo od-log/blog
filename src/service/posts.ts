2558cd738eb7b96b16ea6e8999edf22aa97419b6
OD <kg-te@naver.com>
2023-07-17 01:43:52 +0900

    📝 getPostData에 이전/다음 포스트 데이터 추가

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,5 +1,5 @@
-import path from 'path';
-import { readFile } from 'fs/promises';
+import path from "path";
+import { readFile } from "fs/promises";
 
 export interface Post {
   id: string;
@@ -11,17 +11,20 @@ export interface Post {
   featured: boolean;
 }
 
-export type PostData = Post & { content: string };
+export type PostData = Post & {
+  content: string;
+  next: Post | null;
+  prev: Post | null;
+};
 
-export async function getFeaturedPosts() : Promise<Post[]> {
-  return getAllPosts()
-    .then((posts) => posts.filter((post) => post.featured));
+export async function getFeaturedPosts(): Promise<Post[]> {
+  return getAllPosts().then((posts) => posts.filter((post) => post.featured));
 }
-export async function getAllPosts() : Promise<Post[]> {
-  const filePath = path.join(process.cwd(), 'data', 'posts.json');
-  return readFile(filePath, 'utf-8')
+export async function getAllPosts(): Promise<Post[]> {
+  const filePath = path.join(process.cwd(), "data", "posts.json");
+  return readFile(filePath, "utf-8")
     .then<Post[]>(JSON.parse)
-    .then((posts) => posts.sort((a,b) => (a.date > b.date ? -1 : 1)));
+    .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
 }
 
 export async function getPostItem(path: string): Promise<Post | undefined> {
@@ -30,9 +33,15 @@ export async function getPostItem(path: string): Promise<Post | undefined> {
 }
 
 export async function getPostData(fileName: string): Promise<PostData> {
-  const filePath = path.join(process.cwd(), 'data','posts', `${fileName}.md`);
-  const metadata = await getAllPosts().then(posts => posts.find(post => post.path === fileName));
-  if (!metadata) throw new Error(`${fileName}`);
-  const content = await readFile(filePath, 'utf-8');
-  return {...metadata, content };
+  const filePath = path.join(process.cwd(), "data", "posts", `${fileName}.md`);
+  const posts = await getAllPosts();
+  const post = posts.find((post) => post.path === fileName);
+  const content = await readFile(filePath, "utf-8");
+  if (!post) throw new Error(`${fileName}을 찾을 수 없음.`);
+
+  const index = posts.indexOf(post);
+  const next = index < posts.length - 1 ? posts[index + 1] : null;
+  const prev = index > 0 ? posts[index - 1] : null;
+
+  return { ...post, content, next, prev };
 }
