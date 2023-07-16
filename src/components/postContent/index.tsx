baefe0cf751b8942c42d45efa77260b080980c59
OD <kg-te@naver.com>
2023-07-17 01:42:40 +0900

    ✨ PostContent 추가

---
+++ src/components/postContent/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import MarkDownViewer from "../markDownViewer";
+import Image from "next/image";
+import { PostData } from "@/service/posts";
+
+type Props = {
+  postData: PostData;
+};
+
+export default function PostContent({ postData }: Props) {
+  const { title, path, date, description, content } = postData;
+  return (
+    <article className="rounded-2xl overflow-hidden m-4">
+      <Image
+        className="w-full h-1/6 max-h-[500px]"
+        src={`/images/${path}.jpg`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section className="p-4">
+        <p className="flex items-center justify-end font-semibold ml-2">{date.toString()}</p>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-xl font-bold">{description}</p>
+        <MarkDownViewer content={content}></MarkDownViewer>
+      </section>
+    </article>
+  );
+}
