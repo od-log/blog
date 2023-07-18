54ecbd7a8241ca4a1d4e0e0d48480d831dec26b4
OD <kg-te@naver.com>
2023-07-18 21:08:02 +0900

    📝 Contact페이지 메타데이터 추가

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 import ContactForm from "@/components/contactForm";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Contact Me",
+  description: "오디에게 메일 보내기",
+};
 
 const Links = [{ icon: "깃허브", url: "" }];
 const titleStyle = "text-3xl font-bold pt-2";
