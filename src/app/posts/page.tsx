7dd2ab2f3a6f403c5fb55c0ab260cb0125a18302
OD <kg-te@naver.com>
2023-07-18 21:07:41 +0900

    📝 PostsPage 메타데이터 추가

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,14 +1,16 @@
-import FilterablePosts from '@/components/filterablePosts';
-import { getAllPosts } from '@/service/posts';
-import extractCategories from '@/utils/extractCategories';
+import FilterablePosts from "@/components/filterablePosts";
+import { getAllPosts } from "@/service/posts";
+import extractCategories from "@/utils/extractCategories";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "All Posts",
+  description: "블로그 글들",
+};
 
 export default async function PostsPage() {
   const posts = await getAllPosts();
   const categories = extractCategories(posts);
 
-  return (
-    <FilterablePosts posts={posts} categories={categories} />
-  );
-};
-
-
+  return <FilterablePosts posts={posts} categories={categories} />;
+}
