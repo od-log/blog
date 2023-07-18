ef4ada86b1e68321593cadf95eb8c825b6215bc1
OD <kg-te@naver.com>
2023-07-18 21:07:24 +0900

    📝 generateStaticParams 추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import PostContent from "@/components/postContent";
 import PostPagination from "@/components/postPagination";
-import { getPostData } from "@/service/posts";
+import { getFeaturedPosts, getPostData } from "@/service/posts";
+import { Metadata } from "next";
 
 type Props = {
   params: {
@@ -8,17 +9,32 @@ type Props = {
   };
 };
 
+export async function generateMetadata({ params: { slug } }: Props): Promise<Metadata> {
+  const { title, description } = await getPostData(slug);
+  return {
+    title,
+    description,
+  };
+}
+
 export default async function PostPage({ params: { slug } }: Props) {
   const postData = await getPostData(slug);
   const { next, prev } = postData;
 
   return (
-    <>
+    <article>
       <PostContent postData={postData} />
       <section className="flex shadow-md">
         {prev && <PostPagination post={prev} type={"prev"} />}
         {next && <PostPagination post={next} type={"next"} />}
       </section>
-    </>
+    </article>
   );
+}
+
+export async function generateStaticParams() {
+  const posts = await getFeaturedPosts();
+  return posts.map((post) => ({
+    slug: post.path,
+  }));
 }
