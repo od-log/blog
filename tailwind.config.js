e98fb6c5c08530cb1cf6f47436dc2c2dd9f2d57b
OD <kg-te@naver.com>
2023-07-19 21:59:07 +0900

    💄 custom animation 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        "left-bounce": "left-bounce 0.5s linear infinite alternate",
+        "right-bounce": "right-bounce 0.5s linear infinite alternate",
+      },
+      keyframes: {
+        "left-bounce": {
+          "0%": { "margin-left": "0px" },
+          "100%": { "margin-left": "10px" },
+        },
+        "right-bounce": {
+          "0%": { "margin-left": "0px" },
+          "100%": { "margin-left": "10px" },
+        },
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
