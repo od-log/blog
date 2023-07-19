5dacd9d40007f459ec0b60ab82cda5b2dd1b2930
OD <kg-te@naver.com>
2023-07-19 17:17:02 +0900

    📝 Footer 문구 변경

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
-    <footer className='bg-slate-900 text-white py-2 text-sm text-center'>
-      {"od's log"}
+    <footer className="py-5 text-slate-500 text-sm text-center border-t">
+      <p>{`© 2023 OD's Log. All Rights Reserved.`}</p>
+      <p>{" Built with Next.js and Tailwind CSS | deployed with Vercel"}</p>
     </footer>
-  )
+  );
 }
