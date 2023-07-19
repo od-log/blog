8b1ebe91a58e2107d67e8c6a3cf0b2071de131ed
OD <kg-te@naver.com>
2023-07-20 02:05:31 +0900

    ♻️ li 태그 추가

--- src/components/postsGrid/index.tsx
+++ src/components/postsGrid/index.tsx
@@ -1,16 +1,18 @@
-import { Post } from '@/service/posts';
-import PostCard from '../postCard';
+import { Post } from "@/service/posts";
+import PostCard from "../postCard";
 
 type Props = { posts: Post[] };
 
-export default function PostsGrid({posts} : Props) {
+export default function PostsGrid({ posts }: Props) {
   return (
-    <ul className='grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+    <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {posts.map((post: Post) => {
         return (
-          <PostCard key={post.id} post={post} />
-        )
+          <li key={post.id}>
+            <PostCard post={post} />
+          </li>
+        );
       })}
     </ul>
-  )
+  );
 }
