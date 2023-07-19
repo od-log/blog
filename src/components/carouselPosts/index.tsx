506bfa06b63865a77d4900165e5ea16cec79a82c
OD <kg-te@naver.com>
2023-07-20 00:21:01 +0900

    📝 파일명 소문자로 변경

--- src/components/carouselPosts/index.tsx
+++ src/components/carouselPosts/index.tsx
@@ -1,21 +1,19 @@
-import { getAllPosts } from '@/service/posts'
-import React from 'react'
-import PostCard from '../postCard';
-import MultiCarousel from '../MultiCarousel';
+import { getAllPosts } from "@/service/posts";
+import React from "react";
+import PostCard from "../postCard";
+import MultiCarousel from "../multiCarousel";
 
 export default async function CarouselPosts() {
   const posts = await getAllPosts();
 
   return (
-    <section className='my-4'>
-      <h2 className='text-2xl font-bold m-2'> You may like </h2>
+    <section className="my-4">
+      <h2 className="text-2xl font-bold m-2"> You may like </h2>
       <MultiCarousel>
         {posts.map((post, index) => {
-          return (
-            <PostCard key={post.id} post={post} />
-          )
+          return <PostCard key={post.id} post={post} />;
         })}
       </MultiCarousel>
     </section>
-  )
+  );
 }
