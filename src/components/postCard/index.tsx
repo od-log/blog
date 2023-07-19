1e2e6f5b7d5642c8cc41e1c55d337419a1202d2f
OD <kg-te@naver.com>
2023-07-19 22:04:27 +0900

    💄 postCard 애니메이션 추가

--- src/components/postCard/index.tsx
+++ src/components/postCard/index.tsx
@@ -1,27 +1,29 @@
-import { Post } from '@/service/posts';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react'
+import { Post } from "@/service/posts";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-type Props = { post: Post; }
-export default function PostCard({post : {id, title, description, date, path, category}}: Props) {
+type Props = { post: Post };
+export default function PostCard({ post: { title, description, date, path, category } }: Props) {
   return (
     <Link href={`/posts/${path}`}>
-      <article className='rounded-md overflow-hidden shadow-md hover:shadow-2xl'>
-        <Image
-          className='w-full'
-          src={`/images/${path}.jpg`}
-          alt={title}
-          width={300}
-          height={200}
-        />
-        <div className='flex flex-col items-center p-4'>
-          <time className='self-end'>{date}</time>
-          <h3 className='text-lg font-bold'>{title}</h3>
-          <p className='w-full truncate text-center'>{description}</p>
-          <span className='text-sm rounded-lg bg-yellow-400 px-2 m-2'>{category}</span>
+      <article className="rounded-md overflow-hidden shadow-md hover:shadow-2xl">
+        <div className="overflow-hidden">
+          <Image
+            className="w-full h-48 hover:scale-110 ease-linear duration-300"
+            src={`/images/${path}.jpg`}
+            alt={title}
+            width={272}
+            height={165}
+          />
+        </div>
+        <div className="flex flex-col items-center p-4">
+          <time className="self-end text-slate-600">{date}</time>
+          <h3 className="text-lg font-bold">{title}</h3>
+          <p className="w-full truncate text-center">{description}</p>
+          <span className="text-sm rounded-lg bg-yellow-400 px-2 m-2">{category}</span>
         </div>
       </article>
     </Link>
-  )
+  );
 }
